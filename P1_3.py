9b0a5901e6ccf4ddefa6526b2774db3c7f846399
jaity-teinah <jaityteinah@gmail.com>
2025-04-10 10:48:01 +0530

    Add files via upload

---
+++ P1_3.py
@@ -0,0 +1,17 @@
+from sympy import *
+x = symbols('x')
+eqn =x**3-7*x+5
+n = 10
+print("No. of stages: ", n)
+a = 1
+b = 0
+for i in range(0,n):
+    soll = (a + b) / 2
+    print(soll)
+    sol2 = eqn.subs(x, soll)
+    print(sol2)
+    if sol2 > 0:
+        b = soll
+    if sol2 < 0:
+        a = soll
+print("The approximate real root is", soll)
